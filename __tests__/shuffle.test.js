8f4884f6dc0c7241b90205c6660ec7f083cce130
Cody Fackrell <codyfackrell56@gmail.com>
2023-04-04 20:50:00 -0600

    completed the tests

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,9 +1,13 @@
-// const { shuffle } = require('../src/shuffle')
-// // const bots = require("../src/botsData");
+const shuffle = require('../src/shuffle')
+const bots = require("../src/botsData");
 
 
-// describe('shuffle should...', () => {
-//   test('return an array', () => {
-//     expect(shuffle).toBe('object')
-//   })
-// })
+describe('shuffle should...', () => {
+  test('return an array', () => {
+    expect(shuffle()).toStrictEqual([])
+  })
+
+  test('return array of same length', () => {
+    expect(shuffle(bots)).toHaveLength(10)
+  })
+})
